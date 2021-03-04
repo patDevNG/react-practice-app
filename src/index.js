e2c1950ea956506c5390043818ab1f2efc7ca4d9
Okuns Patrick <cgrleadership@gmail.com>
2021-03-04 13:34:56 +0100

    set up redux and redirect when logged in

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './redux/store'
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
+  <Provider store ={store}>
   <React.StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-   
-  </React.StrictMode>,
+    
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
