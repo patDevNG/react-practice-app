1d2595fb0f555c5efb79d75e850fd7da1d6fe0ce
Okuns Patrick <cgrleadership@gmail.com>
2021-03-11 13:36:21 +0100

    normalized data in the shop reducer

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -4,8 +4,9 @@ import {selectCollection} from '../../redux/shop/shop.selector'
 import CollectionItem from '../../components/collection-items/collection-item.component';
 import './collection.styles.scss' ;
 
- const CollectionPage =({ collection})=>{
-    const {title, items} = collection
+ const CollectionPage =({ collections})=>{
+     console.log(collections)
+    const {title, items} = collections
    return (
        <div className ='collection-page'>
            <h2 className='title'>{title}</h2>
@@ -20,6 +21,6 @@ import './collection.styles.scss' ;
    ) 
 }
 const mapStateToProps = (state,ownProps)=>({
-    collection:selectCollection(ownProps.match.params.collectionId)(state)
+    collections:selectCollection(ownProps.match.params.collectionId)(state)
 })
 export default connect(mapStateToProps)(CollectionPage)
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -2,7 +2,7 @@ import {createSelector} from 'reselect'
 
 const COLLECTION_ID_MAP={
   hats:1,
-  sneaksers:2,
+  sneakers:2,
   jackets:3,
   womens:4,
   mens:5
@@ -16,7 +16,12 @@ const COLLECTION_ID_MAP={
       shop =>shop.collections
   )
 
+  export const selectCollectionForPreview = createSelector(
+      [selectCollections],
+      collections=>Object.keys(collections).map(key=>collections[key])
+  )
+
   export const selectCollection = collectionUrlParam => createSelector(
       [selectCollections],
-      collections => collections.find(collection=>collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+      collections => collections[collectionUrlParam]
   )
